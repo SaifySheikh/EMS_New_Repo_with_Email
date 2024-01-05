8ac44a2b16162a6d50869a1fbe3107a1da69a258
Mohammad Saify Sheikh <121544106+SaifySheikh@users.noreply.github.com>
2024-01-06 00:09:44 +0530

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,17 +1,3 @@
-let arrow = document.querySelectorAll(".arrow");
-  for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=>{
-   let arrowParent = e.target.parentElement.parentElement;
-   arrowParent.classList.toggle("showMenu");
-    });
-  }
-  let sidebar = document.querySelector(".sidebar");
-  let sidebarBtn = document.querySelector(".bx-menu");
-  console.log(sidebarBtn);
-  sidebarBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
-  });
-
 
   function getCurrentDate() {
     const now = new Date();
@@ -100,7 +86,7 @@ const newRequirement = prompt(`Enter new Requirement (Enter "${oldRequirement}" 
                           Company_Name: newCompanyName,
                           Target_Completed: newTarget,
                           Requirement: newRequirement,
-                      }), // Ensure property names match your MongoDB collection
+                      }), 
                   });
 
                   const updatedUser = await response.json();
@@ -121,3 +107,14 @@ const newRequirement = prompt(`Enter new Requirement (Enter "${oldRequirement}" 
       }
   });
 });
+
+
+
+
+function adminLogout() {
+  // Redirect to index.html
+  window.location.replace('index.html');
+}
+
+// Attach the click event to the logout icon
+document.getElementById('admin-logout').addEventListener('click', adminLogout);
